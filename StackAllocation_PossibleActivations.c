31046453126ca9ad7649ca067507f479791a8e04
19C062 Nitin Vinayak <89002023+iamNitinVinayak@users.noreply.github.com>
2022-05-25 17:29:24 +0530

    compilers

---
+++ StackAllocation_PossibleActivations.c
@@ -0,0 +1,12 @@
+enter mainQ
+    enter readArrayO
+    leave readArrayO
+    enter quicksort ( 1 , 9)
+        enter partition ( 1 , 9)
+        leave partition ( l , 9)
+        enter quicksort ( 1 , 3)
+        leave quicksort ( 1 , 3)
+        enter quicksort ( 5 , 9)
+        leave quicksort ( 5 , 9)
+    leave quicksort ( 1 , 9)
+leave main()
